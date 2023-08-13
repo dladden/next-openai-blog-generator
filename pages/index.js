f6782696ba78ff49577c18bce742a2edf9ea5a40
dladden <dladden77@gmail.com>
2023-08-13 18:41:40 -0400

    setting up auth login / logout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
 export default function Home() {
+  const { user } = useUser();
+  console.log(user);
   return (
     <div>
       <h1>TextFlow AI: Homepage</h1>
+      <div>
+        {!!user ? (
+          <>
+            <div>Show all the user info</div>
+            <Link href="/api/auth/logout">logout</Link>
+          </>
+        ) : (
+          <Link href="/api/auth/login">login</Link>
+        )}
+      </div>
     </div>
   );
 }
