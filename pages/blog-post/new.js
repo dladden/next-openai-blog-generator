31914eccf8b8543df0daf0a35e5b98df406d72b0
dladden <dladden77@gmail.com>
2023-08-16 14:32:24 -0400

    setting up page layout rendering

--- pages/blog-post/new.js
+++ pages/blog-post/new.js
@@ -1,3 +1,4 @@
+import { AppLayout } from "@/components/AppLayout";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export default function newPost(props) {
@@ -8,6 +9,17 @@ export default function newPost(props) {
     </div>
   );
 }
+
+/*
+The purpose of the getLayout function is to wrap the page component 
+with the AppLayout component and provide the pageProps to it. 
+This allows you to define a consistent layout structure that surrounds 
+the content of your page.
+*/
+newPost.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
 //this special function runs server-side when requested
 export const getServerSideProps = withPageAuthRequired(() => {
   return {
