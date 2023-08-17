0acd054215286e101c447f76ac931c26393c841d
dladden <dladden77@gmail.com>
2023-08-16 22:22:04 -0400

    more stylistic updates

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,3 +1,6 @@
+import { UseUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+import Link from "next/link";
 /*
 AppLayout will rander the layout and will take on value from children
  */
@@ -10,19 +13,26 @@ export const AppLayout = ({ children }) => {
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col text-white overflow-hidden">
         {/* HEADER & LOGO & NEW-POST */}
-        <div>
+        <div className=" bg-neutral-200 px-4">
           <div>logo</div>
-          <div>new post</div>
-          <div>tokens</div>
+          <Link
+            href="blog-post/new"
+            className="bg-transparent hover:bg-neutral-900 text-neutral-900 font-semibold hover:text-white py-2 px-4 border border-neutral-800 hover:border-transparent rounded tracking-wider w-full block text-center"
+          >
+            new post
+          </Link>
         </div>
         {/* POST LIST */}
-        <div className="flex-1 overflow-auto h-72 bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-gray-200 via-gray-400 to-gray-400">
+        <div className="flex-1 overflow-auto h-72 bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-neutral-200 via-neutral-400 to-neutral-300">
           post list
         </div>
         {/* USER & LOGOUT */}
-        <div>user logout</div>
+        <div className="bg-neutral-200">
+          <Link href="/credit-purchase">tokens</Link>
+          <div>user logout</div>
+        </div>
       </div>
-      <div className="bg-green-500">{children}</div>
+      <div className="">{children}</div>
     </div>
   );
 };
--- pages/credit-purchase.js
+++ pages/credit-purchase.js
@@ -1,12 +1,24 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "@/components/AppLayout";
 
 export default function creditPurchase() {
   return (
     <div>
-      <h1>TextFlow AI: Credit Purchase</h1>
+      <h1> Credit Purchase</h1>
     </div>
   );
 }
+
+/*
+The purpose of the getLayoutFunc is to wrap the page component 
+with the AppLayout component and provide the pageProps to it. 
+This allows you to define a consistent layout structure that surrounds 
+the content of your page.
+*/
+creditPurchase.getLayoutFunc = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
 //this special function runs server-side when requested
 export const getServerSideProps = withPageAuthRequired(() => {
   return {
