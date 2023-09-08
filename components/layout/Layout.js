fb6e940db70cf471fe7c200c2fab8a1464d59ced
dladden <dladden77@gmail.com>
2023-09-07 21:42:46 -0400

    setting up post queering

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -56,12 +56,13 @@ export const Layout = ({ props, children }) => {
           <div className="relative flex-1 overflow-auto h-72 bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-neutral-200 via-neutral-400 to-neutral-300">
             <div>post list post list</div>
           </div>
+
           {/*---------------- USER & LOGOUT ----------------*/}
           <div className="bg-neutral-200">
             <div
               className={`${
-                open && "flex "
-              } items-center gap-2 border-t border-t-black/60 h-[8rem] px-2`}
+                open && "flex"
+              }  gap-2 border-t border-t-black/60 h-[8rem] px-2`}
             >
               <div className="">
                 {!!user ? (
@@ -88,21 +89,23 @@ export const Layout = ({ props, children }) => {
                   <Link href="/api/auth/login">login</Link>
                 )}
               </div>
-            </div>
-            {/*---------------- CREDITS ----------------*/}
-            <div
-              className={`absolute bottom-0 mb-4 left-4 w-auto  ${
-                !open && "w-[3rem]"
-              } duration-200`}
-            >
-              <Link
-                className="bg-transparent hover:bg-neutral-900 text-neutral-900 font-semibold hover:text-white border border-neutral-800 hover:border-transparent rounded text-center flex items-center py-2 px-2 space-x-6"
-                href="/credit-purchase"
+              {/*---------------- CREDITS ----------------*/}
+              <div
+                className={`absolute mb-4 left-4 w-auto  ${
+                  !open && "w-[3rem]"
+                } duration-200`}
               >
-                <WalletIcon className="flex-none w-5 h-6" />
-                <span className={`${!open && "scale-0"} flex-1`}>Credits:</span>
-                <span className={`${!open && "scale-0"} flex-1`}>20</span>
-              </Link>
+                <Link
+                  className="bg-transparent hover:bg-neutral-900 text-neutral-900 font-semibold hover:text-white border border-neutral-800 hover:border-transparent rounded text-center flex items-center py-2 px-2 space-x-6"
+                  href="/credit-purchase"
+                >
+                  <WalletIcon className="flex-none w-5 h-6" />
+                  <span className={`${!open && "scale-0"} flex-1`}>
+                    Credits:
+                  </span>
+                  <span className={`${!open && "scale-0"} flex-1`}>20</span>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
--- pages/blog-post/[postid].js
+++ pages/blog-post/[postid].js
@@ -1,3 +1,6 @@
+import { Layout } from "@/components/layout/Layout";
+import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import clientPromise from "@/lib/mongodb";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 /*
 This is a dynamic root file aka: "[name].js" which allows to have
@@ -10,8 +13,31 @@ export default function Post() {
     </div>
   );
 }
-export const getServerSideProps = withPageAuthRequired(() => {
-  return {
-    props: {},
-  };
+/*
+The purpose of the getLayoutFunc is to wrap the page component 
+with the Layout component and provide the pageProps to it. 
+This allows you to define a consistent layout structure that surrounds 
+the content of your page.
+*/
+Post.getLayoutFunc = function getLayout(page, pageProps) {
+  return <Layout {...pageProps}>{page}</Layout>;
+};
+/**
+ * getServerSideProps is ann async function which will set a function on
+ * an object. ctx is a context of the function. We use it to connect to the
+ * mongodb. we pass the req and res
+ */
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    const userSession = await getSession(ctx.req, ctx.res);
+    const client = await clientPromise;
+    //connecting to the database
+    const db = client.db("textFlow");
+    //Grabbing the post by id
+    const user = await db.collection("users").findOne({
+      auth0Id: userSession.user.sub,
+    });
+    //Queering the post
+    const post = await db.collection("post").findOne({});
+  },
 });
