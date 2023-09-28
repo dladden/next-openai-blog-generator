1e6085c1870e877b927541118d9ade6f64cb1d5c
dladden <dladden77@gmail.com>
2023-09-27 20:36:00 -0400

    set up return data as checkoutSession

--- pages/api/addCredits.js
+++ pages/api/addCredits.js
@@ -60,6 +60,6 @@ export default async function handler(req, res) {
       upsert: true,
     }
   );
-
-  res.status(200).json({ name: 'John Doe' });
+  //return data
+  res.status(200).json({ session: checkoutSession });
 }
